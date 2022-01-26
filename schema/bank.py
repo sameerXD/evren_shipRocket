62fcfcf7e91b776827a7f088e5371deea62e041a
Sachin <sachinkw2107@gmail.com>
2022-01-26 22:58:10 +0530

    connected bank requirements in kyc, added bank apis

---
+++ schema/bank.py
@@ -0,0 +1,8 @@
+schema = {
+    'user_id':{'type':'integer','required': True},
+    'bank_name':{'type':'string','required': True},
+    'account_type':{'type':'string','required': True},
+    'account_number':{'type':'string','required': True},
+    'beneficiary_name':{'type':'string','required': True},
+    'ifsc_code':{'type':'string','required': True},
+}
--- schema/kyc.py
+++ schema/kyc.py
@@ -3,9 +3,14 @@ schema = {
     'pancard':{'type':'string','required': True},
     'pancard_url':{'type':'string','required': True},
     'company_type':{'type':'string','required': True},
-    'business_proof_used':{'type':'string','required': True},
-    'business_proof_url':{'type':'string','required': True},
+    'aadhar_card':{'type':'string','required': True},
+    'aadhar_card_url':{'type':'string','required': True},
     'has_gst_number':{'type':'integer','required': True},
     'gst_number':{'type':'string'},
+    'bank_name':{'type':'string','required': True},
+    'account_type':{'type':'string','required': True},
+    'account_number':{'type':'string','required': True},
+    'beneficiary_name':{'type':'string','required': True},
+    'ifsc_code':{'type':'string','required': True},
 }
 
