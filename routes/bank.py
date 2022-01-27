e9ae8f9bb6293a39314675e8b0b8dc03a4bd3c85
Sachin <sachinkw2107@gmail.com>
2022-01-27 14:00:47 +0530

    set primary bank

--- routes/bank.py
+++ routes/bank.py
@@ -1,4 +1,4 @@
-from controllers.bank import add_bank_account, show_bank_accounts
+from controllers.bank import add_bank_account, show_bank_accounts, set_primary_account
 from flask import Blueprint , request
 from utils.security import token_required
 
@@ -6,7 +6,7 @@ bank_page = Blueprint('bank',__name__)
 
 @bank_page.route("/api/add/bank/account",methods=["POST"])
 @token_required
-def store(user):
+def bank(user):
     if request.method == "POST":
         request.json["user_id"] = user.id
         return add_bank_account(request.json)
@@ -14,6 +14,13 @@ def store(user):
 
 @bank_page.route("/api/show/bank/accounts",methods=["GET"])
 @token_required
-def myStores(user):
+def myBankAccounts(user):
     if request.method == "GET":
         return show_bank_accounts(user.id)
+
+
+@bank_page.route("/api/set/primary/bank/<bank_id>", methods=["PUT"])
+@token_required
+def set_as_primary(user, bank_id):
+    if request.method == "PUT":
+        return set_primary_account(user.id, bank_id)
