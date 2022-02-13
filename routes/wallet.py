1fedf457749d2dbc46d404d0798ded1cd58a79d9
bob <samdragneal@gmail.com>
2022-02-13 10:32:33 +0530

    cors added

--- routes/wallet.py
+++ routes/wallet.py
@@ -10,24 +10,24 @@ from flask_cors import CORS
 
 cors = CORS()
 
-wallet = Blueprint('wallet',__name__)
-cors.init_app(wallet, resources={r"/*": {"origins": "*", "supports_credentials": True}})
+wallet_page = Blueprint('wallet',__name__)
+cors.init_app(wallet_page, resources={r"/*": {"origins": "*", "supports_credentials": True}})
 
-@wallet.route('/api/wallet/createOrder', methods=["GET"])
+@wallet_page.route('/api/wallet/createOrder', methods=["GET"])
 def create_order():
     return homepage()
 
-@wallet.route('/api/wallet/paymenthandler/', methods=["POST"])
+@wallet_page.route('/api/wallet/paymenthandler/', methods=["POST"])
 @token_required
 def handle_payment(user):
     return paymenthandler(request.form,user)
 
-@wallet.route('/api/wallet/getBallance/', methods=["GET"])
+@wallet_page.route('/api/wallet/getBallance/', methods=["GET"])
 @token_required
 def get_wallet_ballance(user):
     return get_ballance(user)
 
 
-@wallet.route('/api/wallet/testTrans', methods=["GET"])
+@wallet_page.route('/api/wallet/testTrans', methods=["GET"])
 def create_wallet_user():
     return requests.get("https://api.genderize.io/?name=luc").content
